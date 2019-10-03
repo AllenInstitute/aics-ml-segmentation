4895c0136c89e0b79be7c3ac63d7ba44844d3b85
Filip Sluzewski <filip.sluzewski@dev-aics-fsp-001.corp.alleninstitute.org>
2019-10-03 16:13:47 -0700

    copying useful function from mitotic gui

--- aicsmlsegment/bin/curator/curator_gui.py
+++ aicsmlsegment/bin/curator/curator_gui.py
@@ -38,59 +38,117 @@ import pandas
 
 
 
-def main():
-    layout = [
-        [sg.Text("User: ", key="userText", size=(20, 1))],
-        [sg.Input('', key="username", size=(20, 1))],
-        [sg.Text("Dataset CSV: ", key="csvText", size=(20, 1))],
 
-        [sg.Input("/allen/aics/assay-dev/computational/data/mitotic_annotation_gui/pseudo_real_data/gui_sample_dataset.csv", key="csvPath", size=(40, 1)), 
-         sg.FileBrowse(key='csvSelect', file_types=(('CSV Files','*.csv'),), initial_folder='/allen/aics/assay-dev/computational/data/'),
-         sg.Text("", key="loadError", text_color='red', size=(60, 1))],
+layout = [
 
-        [sg.Button("Load DSDB", key="b_load"), 
-         sg.Text('Cell ID: ', key='fn', size=(9, 1)),
-         sg.Text('', key='filename', size=(20, 1))],
+    [sg.Graph(canvas_size=(500, 500),graph_bottom_left=(0, 500),graph_top_right=(500, 0),key="rawGraph",change_submits=True,drag_submits=False),
+    sg.Graph(canvas_size=(500, 500),graph_bottom_left=(0, 500),graph_top_right=(500, 0),key="seg1Graph",change_submits=True,drag_submits=False),
+    sg.Graph(canvas_size=(500, 500),graph_bottom_left=(0, 500),graph_top_right=(500, 0),key="seg2Graph",change_submits=True,drag_submits=False)],
 
-        [sg.Graph(canvas_size=(100, 400),graph_bottom_left=(0, 400),graph_top_right=(400, 0),key="graph_dna_yz",change_submits=True,drag_submits=True),
-        sg.Graph(canvas_size=(400, 400),graph_bottom_left=(0, 400),graph_top_right=(400, 0),key="graph_dna_xy",change_submits=True,drag_submits=True),
-        sg.Graph(canvas_size=(400, 400),graph_bottom_left=(0, 400),graph_top_right=(400, 0),key="graph_mem_xy",change_submits=True,drag_submits=True),
-        sg.Graph(canvas_size=(100, 400),graph_bottom_left=(0, 400),graph_top_right=(100, 0),key="graph_mem_yz",change_submits=True,drag_submits=True)],
+    
+    [sg.Frame(layout=[[sg.Button('Exclusion Mask', key='exMask', enable_events=True),
+                       sg.Button('Merging Mask', key='mergeMask', enable_events=True)],
+                      [sg.Button('Delete Last', key='delExMask', enable_events=True),
+                       sg.Button('Delete Last', key='delMergeMask', enable_events=True)]],
+            title='Create New Mask',
+            relief=sg.RELIEF_SUNKEN),
+    sg.Frame(layout=[[sg.Radio(text='Middle Slice', key='mid', group_id='projMethod', default=True, enable_events=True), 
+                    sg.Radio(text='Max Projection', key='max', group_id='projMethod', enable_events=True), 
+                    sg.Radio(text='Average Projection', key='ave', group_id='projMethod',enable_events=True)]],
+            title='Projection Options',
+            relief=sg.RELIEF_SUNKEN),
+    sg.Checkbox('Enhance Contrast', key='enhCon', enable_events=True),
+    sg.Button('Next Image', key='next', enable_events=True),
+    sg.Button('Save', key='save', enable_events=True),
+    sg.Exit()]
+]
 
-        [sg.Graph(canvas_size=(100, 100),graph_bottom_left=(0, 100),graph_top_right=(100, 0),key="graph_dna_blank",change_submits=True,drag_submits=True),
-        sg.Graph(canvas_size=(400, 100),graph_bottom_left=(0, 400),graph_top_right=(400, 0),key="graph_dna_xz",change_submits=True,drag_submits=True),
-        sg.Graph(canvas_size=(400, 100),graph_bottom_left=(0, 400),graph_top_right=(400, 0),key="graph_mem_xz",change_submits=True,drag_submits=True),
-        sg.Graph(canvas_size=(100, 100),graph_bottom_left=(0, 100),graph_top_right=(100, 0),key="graph_mem_blank",change_submits=True,drag_submits=True)],
+window = sg.Window("Curator", layout)
 
-        [sg.Frame(layout=[[sg.Checkbox('Enhance Contrast', key='enhCon', default=False, enable_events=True),
-                            sg.Checkbox('Median Filter', key='medFilt',default=True, enable_events=True),
-                            sg.Checkbox('Gaussian Filter', key='gFilt', default=False, enable_events=True),
-                            sg.Checkbox(text='Show Segmentation', key='showSeg', default=False, enable_events=True)]],
-                title='Image Options',
-                relief=sg.RELIEF_SUNKEN),
-        sg.Frame(layout=[[sg.Radio(text='Middle Slice', key='mid', group_id='projMethod', default=True, enable_events=True), 
-                        sg.Radio(text='Max Projection', key='max', group_id='projMethod', enable_events=True), 
-                        sg.Radio(text='Average Projection', key='ave', group_id='projMethod',enable_events=True)]],
-                title='Projection Options',
-                relief=sg.RELIEF_SUNKEN)],
 
-        [sg.Text("", key="info", size=(60, 1))],
-        [
-            sg.Button('<<', key='bbf'),
-            sg.Button("<", key="bb"),
-            sg.Button(">", key="bf"),
-            sg.Button(">>", key="bff"),
-            sg.Text("", key="spacer", size=(5, 1)),
-            sg.Button("0", key="b0"),
-            sg.Button("1", key="b1"),
-            sg.Button("2", key="b2"),
-            sg.Button("3", key="b3"),
-            sg.Button("4", key="b4"),
-            sg.Frame(layout=[[sg.Checkbox(" UNSURE", key="unsure", default=False, enable_events=True)]], title='',relief=sg.RELIEF_SUNKEN),
-            sg.Frame(layout=[[sg.Checkbox("Activate Hotkeys", key="hotkeysActive", default=False, enable_events=True, disabled=True)]], title='',relief=sg.RELIEF_SUNKEN),
-            sg.Text("Current Image:", key="currImText", size=(15, 1)),
-            sg.Text("", key="currIm", size=(10, 1)),
-            sg.Button("3D", key="b3d"),
-            sg.Button("save", key="bs")
-        ]
-    ]
+
+#################################
+### Button Functions
+#################################
+
+def EnhanceContrast(img,target_shape=(100,100)):
+    if window.Element('enhCon').Get():
+        qinf, qsup = np.percentile(img, [5,95])
+        img = np.clip(img,qinf,qsup)
+        img = (255*(img-qinf)/(qsup-qinf))
+    else:
+        im_min = np.amin(img)
+        im_max = np.amax(img-im_min)
+        img = (img-im_min)/im_max*255
+
+    return img
+
+
+
+
+
+#################################
+### GUI Display Functions
+#################################
+
+def displayImage(img,channel,dim,segChannel,target_shape,config,graph,scale=None):
+    graph.Erase()
+
+    img = projectStack(img,channel,dim)
+    img = EnhanceContrast(img)
+
+    if scale is None:
+        ratios = np.divide(target_shape,np.shape(img))
+        scale = np.amin(ratios)
+
+    new_size = np.multiply(np.shape(img), scale).astype(np.uint64)
+    new_size = (new_size[0], new_size[1])
+    img = resize(img,new_size)
+
+    img = numpy2bytes(img)
+    graph.DrawImage(data=img, location=(0,0))
+    return scale
+
+
+
+def numpy2bytes(img):
+    #must be used to display images without saving
+    pil_img = Image.fromarray(img)
+    buff = BytesIO()
+    pil_img.save(buff, format="GIF")
+    return base64.b64encode(buff.getvalue()).decode("utf-8")
+
+
+def projectStack(img_full, channel, dim, isSeg=False):
+    #outputs middle slice of stack along axis dim
+    #method assumes ZCYX dimension arrangement
+    img = img_full[channel,:,:,:]
+    if dim==2:
+        img = np.transpose(img, (1,0,2))
+    elif dim==3:
+        img = np.transpose(img, (2,1,0))
+
+    if window.Element('mid').Get():
+        numSlices = np.shape(img)[0]
+        mid = np.floor(numSlices/2).astype(np.uint8)
+        img = img[mid,:,:]
+    elif window.Element('max').Get():
+        img = np.max(img, axis=0)
+    elif window.Element('ave').Get():
+        if not isSeg:
+            img = np.mean(img, axis=0)
+        else:
+            img = np.max(img, axis=0)
+
+    return img
+
+
+
+
+###################################
+### Button Event Handling
+###################################
+
+
+while True:
+    event, values = window.Read()
