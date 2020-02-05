fix the image normalization and cleaning up code

aicsmlsegment/model_utils.py
@@ -9,7 +9,7 @@ import sys
 
 from aicsmlsegment.utils import get_logger
 
-SUPPORTED_MODELS = ['unet_xy_zoom', 'unet_xy', 'unet_2d', 'unet_2d_deep', 'deeplabV3', 'deeplabV3_imagenet','deeplabV3plus', 'deeplabV3plus_imagenet']
+SUPPORTED_MODELS = ['unet_xy_zoom', 'deeplabV3', 'deeplabV3_imagenet','deeplabV3plus', 'deeplabV3plus_imagenet']
 
 def weights_init(m):
     classname = m.__class__.__name__
@@ -253,12 +253,6 @@ def build_model(config):
     elif name =='unet_xy_zoom':
         from aicsmlsegment.Net3D.unet_xy_enlarge import UNet3D as DNN
         model = DNN(config['nchannel'], config['nclass'], model_config.get('zoom_ratio',3))
-    elif name == 'unet_2d':
-        from aicsmlsegment.Net2D.uNet_2D import UNet2D as DNN
-        model = DNN(config['nchannel'], config['nclass'])
-    elif name == 'unet_2d_deep':
-        from aicsmlsegment.Net2D.uNet_2D_deep import UNet2D as DNN
-        model = DNN(config['nchannel'], config['nclass'])
     elif name == 'deeplabV3': # deeplab without pre-training
         from aicsmlsegment.Net2D.deeplabV3 import deeplabV3 as DNN
         model = DNN(config['nclass'],False)
@@ -280,14 +274,12 @@ def build_model(config):
 
 
 def apply_on_full_image(model, input_img, softmax, args):
-
-    from PIL import Image
+    '''
+    Inference on full image wiht runtime augmentation
+    '''
     print('doing runtime augmentation')
-    #import pathlib
-    #from aicsimageio import omeTifWriter
 
     input_img_aug = input_img.copy()
-    # input_img_aug = np.expand_dims(input_img_aug, axis=0)
 
     flip1 = np.flipud(input_img_aug)
     out1 = model_inference_full_img(model, flip1, softmax, args)
@@ -302,19 +294,18 @@ def apply_on_full_image(model, input_img, softmax, args):
     out4 = model_inference_full_img(model, flip4, softmax, args)
 
     out = (np.flipud(out1) + np.fliplr(out2) + np.flipud(np.fliplr(out3)) + np.fliplr(np.flipud(out4)))/4
-    # import pdb; pdb.set_trace()
     return out
 
 def model_inference_full_img(model, input_img, softmax, args):
+    '''
+    Instead of do inference on patch, inference on full image.
+    '''
     model.eval()
     input_img = np.expand_dims(input_img, axis=0)
     with torch.no_grad():
-        # if the image and trained model is 2D
-        # for deeplab################################
         input_img_stack = np.stack([input_img,input_img, input_img],axis=1)[0,...]
         input_img_tensor = torch.from_numpy(input_img_stack).float()
         tmp_out = model(Variable(input_img_tensor.cuda()).unsqueeze(0))
-        ###########################################################3
 
         prob = softmax(tmp_out)
         out_flat_tensor = prob.cpu().data.float()

aicsmlsegment/utils.py
@@ -6,12 +6,12 @@ from aicsimageprocessing import resize
 import os
 from scipy import ndimage as ndi
 from scipy import stats
+from scipy.ndimage.morphology import binary_opening, binary_dilation, binary_erosion
+from skimage.morphology import remove_small_objects, erosion, ball, dilation, remove_small_holes
+from skimage.measure import label
 import argparse
 
 import yaml
-
-# debug
-import pdb
 
 def load_config(config_path):
     import torch
@@ -63,90 +63,88 @@ def background_sub(img, r):
     return struct_img
 
 def input_normalization(img, args):
-    #from aicsimageio import omeTifWriter
-    #writer = omeTifWriter.OmeTifWriter('/allen/aics/assay-dev/Segmentation/DeepLearning/DNA_Labelfree_Evaluation/final_evaluation/test_before_norm.tiff')
-    #writer.save(img[0,:,:,:])
-    nchannel = img.shape[0]
-    args.Normalization = int(args.Normalization)
-
-    # if the model is 2D
-    if len(img.shape) == 3: # check if the image is 2D
-        struct_img = img[0,:,:]
-        struct_img = simple_norm(struct_img, 1, 6)
-        return struct_img
-
+    if len(img.shape) == 2: # if Image is 2D
+        nchannel = 1
+        imag2D = True
     else:
-        for ch_idx in range(nchannel):
+        nchannel = img.shape[0]
+    args.Normalization = int(args.Normalization)
+    for ch_idx in range(nchannel):
+        if imag2D:
+            struct_img = img
+        else:
             struct_img = img[ch_idx,:,:,:] # note that struct_img is only a view of img, so changes made on struct_img also affects img
-            if args.Normalization == 0: # min-max normalization
-                struct_img = (struct_img - struct_img.min() + 1e-8)/(struct_img.max() - struct_img.min() + 1e-7)
-            elif args.Normalization == 1: # mem: DO NOT CHANGE (FIXED FOR CAAX PRODUCTION)
-                m,s = stats.norm.fit(struct_img.flat)
-                strech_min = max(m - 2*s, struct_img.min())
-                strech_max = min(m + 11 *s, struct_img.max())
-                struct_img[struct_img>strech_max]=strech_max
-                struct_img[struct_img<strech_min]=strech_min
-                struct_img = (struct_img- strech_min + 1e-8)/(strech_max - strech_min + 1e-8)
-                img[ch_idx,:,:,:] = struct_img[:,:,:]
-            elif args.Normalization == 2: # nuc
-                #struct_img = simple_norm(struct_img, 2.5, 10, 1000, 300)
-                struct_img = simple_norm(struct_img, 2.5, 10)
-                img[ch_idx,:,:,:] = struct_img[:,:,:]
-            elif args.Normalization == 4:
-                struct_img = simple_norm(struct_img, 1, 15)
-                img[ch_idx,:,:,:] = struct_img[:,:,:]
-            elif args.Normalization == 7: # cardio_wga
-                struct_img = simple_norm(struct_img, 1, 6)
-                img[ch_idx,:,:,:] = struct_img[:,:,:]
-            elif args.Normalization == 10: # lamin hipsc, DO NOT CHANGE (FIXED FOR LAMNB1 PRODUCTION)
-                img_valid = struct_img[struct_img>4000]
-                m,s = stats.norm.fit(img_valid.flat)
-                m,s = stats.norm.fit(struct_img.flat)
-                strech_min = struct_img.min()
-                strech_max = min(m + 25 *s, struct_img.max())
-                struct_img[struct_img>strech_max]=strech_max
-                struct_img = (struct_img- strech_min + 1e-8)/(strech_max - strech_min + 1e-8)
-                img[ch_idx,:,:,:] = struct_img[:,:,:]
-            elif args.Normalization == 12: # nuc
-                struct_img = background_sub(struct_img,50)
-                struct_img = simple_norm(struct_img, 2.5, 10)
-                img[ch_idx,:,:,:] = struct_img[:,:,:]
-                print('subtracted background')
-            elif args.Normalization == 11: 
-                struct_img = background_sub(struct_img,50)
-                #struct_img = simple_norm(struct_img, 2.5, 10)
-                img[ch_idx,:,:,:] = struct_img[:,:,:]
-            elif args.Normalization == 13: # cellmask
-                struct_img[struct_img>10000] = struct_img.min()
-                struct_img = background_sub(struct_img,50)
-                struct_img = simple_norm(struct_img, 2, 11)
-                img[ch_idx,:,:,:] = struct_img[:,:,:]
-            elif args.Normalization == 14:
-                struct_img = simple_norm(struct_img, 1, 10)
-                img[ch_idx,:,:,:] = struct_img[:,:,:]
-            elif args.Normalization == 15: # lamin
-                struct_img[struct_img>4000] = struct_img.min()
-                struct_img = background_sub(struct_img,50)
-                img[ch_idx,:,:,:] = struct_img[:,:,:]
-            elif args.Normalization == 16: # lamin/h2b
-                struct_img = background_sub(struct_img,50)
-                struct_img = simple_norm(struct_img, 1.5, 6)
-                img[ch_idx,:,:,:] = struct_img[:,:,:]
-            elif args.Normalization == 17: # lamin
-                struct_img = background_sub(struct_img,50)
-                struct_img = simple_norm(struct_img, 1, 10)
-                img[ch_idx,:,:,:] = struct_img[:,:,:]
-            elif args.Normalization == 18: # h2b
-                struct_img = background_sub(struct_img,50)
-                struct_img = simple_norm(struct_img, 1.5, 10)
-                img[ch_idx,:,:,:] = struct_img[:,:,:]
-            else:
-                print('no normalization recipe found')
-                quit()
-        return img
+
+        if args.Normalization == 0: # min-max normalization
+            struct_img = (struct_img - struct_img.min() + 1e-8)/(struct_img.max() - struct_img.min() + 1e-7)
+        elif args.Normalization == 1: # mem: DO NOT CHANGE (FIXED FOR CAAX PRODUCTION)
+            m,s = stats.norm.fit(struct_img.flat)
+            strech_min = max(m - 2*s, struct_img.min())
+            strech_max = min(m + 11 *s, struct_img.max())
+            struct_img[struct_img>strech_max]=strech_max
+            struct_img[struct_img<strech_min]=strech_min
+            struct_img = (struct_img- strech_min + 1e-8)/(strech_max - strech_min + 1e-8)
+            img[ch_idx,:,:,:] = struct_img[:,:,:]
+        elif args.Normalization == 2: # nuc
+            #struct_img = simple_norm(struct_img, 2.5, 10, 1000, 300)
+            struct_img = simple_norm(struct_img, 2.5, 10)
+            img[ch_idx,:,:,:] = struct_img[:,:,:]
+        elif args.Normalization == 4:
+            struct_img = simple_norm(struct_img, 1, 15)
+            img[ch_idx,:,:,:] = struct_img[:,:,:]
+        elif args.Normalization == 7: # cardio_wga
+            struct_img = simple_norm(struct_img, 1, 6)
+            img[ch_idx,:,:,:] = struct_img[:,:,:]
+        elif args.Normalization == 10: # lamin hipsc, DO NOT CHANGE (FIXED FOR LAMNB1 PRODUCTION)
+            img_valid = struct_img[struct_img>4000]
+            m,s = stats.norm.fit(img_valid.flat)
+            m,s = stats.norm.fit(struct_img.flat)
+            strech_min = struct_img.min()
+            strech_max = min(m + 25 *s, struct_img.max())
+            struct_img[struct_img>strech_max]=strech_max
+            struct_img = (struct_img- strech_min + 1e-8)/(strech_max - strech_min + 1e-8)
+            img[ch_idx,:,:,:] = struct_img[:,:,:]
+        elif args.Normalization == 12: # nuc
+            struct_img = background_sub(struct_img,50)
+            struct_img = simple_norm(struct_img, 2.5, 10)
+            img[ch_idx,:,:,:] = struct_img[:,:,:]
+            print('subtracted background')
+        elif args.Normalization == 11: 
+            struct_img = background_sub(struct_img,50)
+            #struct_img = simple_norm(struct_img, 2.5, 10)
+            img[ch_idx,:,:,:] = struct_img[:,:,:]
+        elif args.Normalization == 13: # cellmask
+            struct_img[struct_img>10000] = struct_img.min()
+            struct_img = background_sub(struct_img,50)
+            struct_img = simple_norm(struct_img, 2, 11)
+            img[ch_idx,:,:,:] = struct_img[:,:,:]
+        elif args.Normalization == 14:
+            struct_img = simple_norm(struct_img, 1, 10)
+            img[ch_idx,:,:,:] = struct_img[:,:,:]
+        elif args.Normalization == 15: # lamin
+            struct_img[struct_img>4000] = struct_img.min()
+            struct_img = background_sub(struct_img,50)
+            img[ch_idx,:,:,:] = struct_img[:,:,:]
+        elif args.Normalization == 16: # lamin/h2b
+            struct_img = background_sub(struct_img,50)
+            struct_img = simple_norm(struct_img, 1.5, 6)
+            img[ch_idx,:,:,:] = struct_img[:,:,:]
+        elif args.Normalization == 17: # lamin
+            struct_img = background_sub(struct_img,50)
+            struct_img = simple_norm(struct_img, 1, 10)
+            img[ch_idx,:,:,:] = struct_img[:,:,:]
+        elif args.Normalization == 18: # h2b
+            struct_img = background_sub(struct_img,50)
+            struct_img = simple_norm(struct_img, 1.5, 10)
+            img[ch_idx,:,:,:] = struct_img[:,:,:]
+        elif args.Normalization == 19: # 2D cardio
+            img = simple_norm(struct_img, 1, 6)
+        else:
+            print('no normalization recipe found')
+            quit()
+    return img
 
 def image_normalization(img, config):
-
     if type(config) is dict:
         ops = config['ops']
         nchannel = img.shape[0]
@@ -240,3 +238,33 @@ def get_logger(name, level=logging.INFO):
     logger.addHandler(stream_handler)
 
     return logger
+
+def post_processing_2d(img):
+    '''
+    Post processing for 2D
+    '''
+    ball_mat_open = ball_matrix(5)
+    ball_mat_hole = ball_matrix(5)
+    ball_mat_dilation = ball_matrix(8)
+    ball_mat_erosion = ball_matrix(5)
+
+    processed_img = binary_opening(img, structure=ball_mat_open).astype(np.int)
+    processed_img = binary_dilation(processed_img, structure=ball_matrix(2),iterations=2)
+
+    object_mat = label(processed_img) # object detection
+
+    # process per individual object
+    final_seg = np.zeros(processed_img.shape)
+    for individual_label in np.unique(object_mat):
+        temp = (object_mat==individual_label)*1
+        temp = binary_dilation(temp, structure=ball_mat_dilation)
+        temp = binary_erosion(temp, structure=ball_mat_erosion)
+        temp = remove_small_holes(temp, area_threshold=700)*1
+
+        final_seg[temp == 1] = individual_label
+    return final_seg
+
+def ball_matrix(r):
+    # making 2D ball shape matrix
+    ball_mat = ball(r)
+    return np.amax(ball_mat, axis = 0)